43a5a3017fc413431f9dde0e1e7bc1a659f252e5
Ranjan Kumar <ranjank326@gmail.com>
2021-12-30 19:43:25 +0000

    added react feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  #root to: 'home#index'
   resources :secret_menu_items, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
